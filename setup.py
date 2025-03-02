4c3cc0849c918c96f7b2a4a2cefa2cc570180338
Bruno Lago <teolupus@gmail.com>
2025-03-02 16:33:07 +1300

    Supported Versions
    Updated the Python versions supported by the library

--- setup.py
+++ setup.py
@@ -21,16 +21,21 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Topic :: Multimedia :: Sound/Audio",
         "Topic :: Scientific/Engineering :: Physics",
     ],
     packages=find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     entry_points={
         "console_scripts": [
-            "svarascala=svarascala.__main__:main",
+            "svarascala=svarascala.main:main",
         ],
     },
 )
